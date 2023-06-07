c801f58b77558d870af2fbaeb1ceca37aab0ba03
Toandone <toanptde160263@fpt.edu.vn>
2023-06-07 13:58:41 +0700

    Add search product, fix star

--- wwwroot/js/user/additionm.js
+++ wwwroot/js/user/additionm.js
@@ -1,18 +1,27 @@
 //Load product by category
-function loadProduct(categoryId, thisitem) {
+function loadProduct(thisitem) {
 
     var activeCategory = document.querySelector(".nav-link.active");
-    activeCategory.classList.remove("active");
 
-    thisitem.classList.add("active");
+    if (thisitem != null) {
+        activeCategory.classList.remove("active");
+        thisitem.classList.add("active");
+        activeCategory = document.querySelector(".nav-link.active");
+    }
+
+    var searchbar = document.querySelector(".search-bar");
 
     var productRows = document.getElementsByClassName("product-item");
+    var categoryId = activeCategory.getAttribute("data-categoryid");
 
     for (var i = 0; i < productRows.length; i++) {
         var row = productRows[i];
         var category = row.getAttribute("data-category");
 
-        if (categoryId == 0 || category == categoryId) {
+        var productName = row.querySelector(".product-name").innerHTML;
+
+        if ((categoryId == 0 || category == categoryId)
+            && Diacritics.clean(productName).toLowerCase().includes(Diacritics.clean(searchbar.value).toLowerCase())) {
             row.style.display = "";
         } else {
             row.style.display = "none";
